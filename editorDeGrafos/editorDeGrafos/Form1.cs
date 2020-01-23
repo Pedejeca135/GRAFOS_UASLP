51bca4aa3f4784a2414947224200a7e002b8c75e
Pedejeca135 <pjco90@hotmail.com>
2020-01-23 16:13:58 -0600

    el esqueleto del proyecto y el gitignore

---
+++ editorDeGrafos/editorDeGrafos/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace editorDeGrafos
+{
+    public partial class Form1 : Form
+    {
+        List<Node> nodeList;
+        List<Edge> edgeList;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }//Form.
+
+    public class Node
+    {
+        Coordenate position;
+        Color color;
+
+
+        public Node(Coordenate position)
+        {
+            this.position = position;
+        }
+
+        public void Draw()
+        {
+
+        }
+
+    }
+
+    public class Edge
+    {
+        Coordenate a;
+        Coordenate b;
+        int weight;
+        int direction;
+
+        public Edge()
+        {
+
+        }
+
+
+    }
+
+    public class Coordenate
+    {
+        int x;
+        int y;
+
+        public Coordenate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public class WeightMatrixGraph
+    {
+
+    }
+
+    public class adjacencyList
+    {
+
+    }
+
+
+
+
+
+
+
+
+}//namespace.
