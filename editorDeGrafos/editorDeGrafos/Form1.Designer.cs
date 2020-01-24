9add6eee4db929348ad04120d91a0bf9d0d0604b
Pedejeca135 <pjco90@hotmail.com>
2020-01-24 08:10:05 -0600

    piensatelo bien muchacho(haciendo los Nodos

--- editorDeGrafos/editorDeGrafos/Form1.Designer.cs
+++ editorDeGrafos/editorDeGrafos/Form1.Designer.cs
@@ -28,22 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Save = new System.Windows.Forms.ToolStripLabel();
+            this.Load = new System.Windows.Forms.ToolStripLabel();
+            this.New = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Save,
+            this.Load,
+            this.New});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Save
+            // 
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(31, 22);
+            this.Save.Text = "Save";
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Load
+            // 
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(33, 22);
+            this.Load.Text = "Load";
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // New
+            // 
+            this.New.Name = "New";
+            this.New.Size = new System.Drawing.Size(31, 22);
+            this.New.Text = "New";
+            this.New.Click += new System.EventHandler(this.New_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.Text = "GraphEditor";
+           this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel Save;
+        private System.Windows.Forms.ToolStripLabel Load;
+        private System.Windows.Forms.ToolStripLabel New;
     }
 }
 
--- editorDeGrafos/editorDeGrafos/Form1.cs
+++ editorDeGrafos/editorDeGrafos/Form1.cs
@@ -14,85 +14,211 @@ namespace editorDeGrafos
     {
         List<Node> nodeList;
         List<Edge> edgeList;
-
-
+        Node selectedNode;
+        public int generalRadius;
 
         public Form1()
         {
             InitializeComponent();
+            generalRadius = 5; //Here you decide the size of the nodes
+            nodeList = new List<Node>();
+            edgeList = new List<Edge>();
+            selectedNode = new Node();
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)//if mouse button pressed is left.
+            {
+                foreach (Node oneNode in nodeList)
+                { 
 
+                    if(e.X > oneNode.Position.X - oneNode.Radius //for conditions in oreder to determine wheter or not , a click hit the specific node
+                        && e.X < oneNode.Position.X + oneNode.Radius
+                        && e.Y < oneNode.Position.Y + oneNode.Radius
+                        && e.Y > oneNode.Position.Y - oneNode.Radius)
+                    {
+                        if(oneNode.SelectedBool)
+                        {
+                            if(oneNode.Status == 3)// in the third state
+                            {
+                               oneNode.Status = 0;//change to the original state.
+                               oneNode.COLOR = Color.Black;//change to black color(original state).
+
+                            }
+                            else
+                            {
+                                if (oneNode.Status == 2)//selected more than one time(second State).
+                                {
+                                    oneNode.Status = 3;//change to thid state for directed links.
+                                    oneNode.COLOR = Color.Red;//change to red color(third state).
+                                }
+                                else
+                                {
+                                    if (oneNode.Status == 1)//selected one time.
+                                    {
+                                        oneNode.Status = 2;//change to the second selected State.
+                                        oneNode.COLOR = Color.Blue;//change to blue color to indicate the status(can do undirected Edges).
+                                    }
+                                    else // is not selected, (Status == 0).
+                                    {
+                                        oneNode.Status = 1;//change to the first selected state.
+                                        oneNode.COLOR = Color.Green;//change to green color to indicate the status(can move).
+                                    }
+                                }
+                            }
+                           
+                        }
+                        else // is tryng to do a link between nodes
+                        {
+
+                        }
+                      
+                    }
+                }
+            }
+            else
+            {
+
+            }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Save_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void New_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public class Node
+        {
+            Coordenate position;
+            Color color;
+            int radiusLenght;
+            Boolean justSelected;
+            int selected;
+            int index;
+
+            public Node(Coordenate position, int radius, int index)
+            {
+                this.position = position;
+                this.radiusLenght = radius;
+                justSelected = false;
+                selected = 0;
+                this.index = index;//ID of the node
+            }
+            public Node() //default constructor of the class Node
+            {
+
+            }
+
+            /*******************************************************
+             *               Geters and seters(Begin)              *
+             *******************************************************/
+
+            public Coordenate Position
+            {
+                get { return this.position; }
+                set { this.position = value; }
+            }
+
+            public int Radius
+            {
+                get { return this.radiusLenght; }
+                set { this.radiusLenght = value; }
+            }
+
+            public int Status
+            {
+                get { return selected; }
+                set { selected = value; }
+            }
+
+            public Color COLOR
+            {
+                get { return this.color; }
+                set { this.color = value; }
+            }
+
+            public Boolean SelectedBool
+            {
+                get { return this.justSelected; }
+                set { this.justSelected = value; }
+            }
+            /*******************************************************
+             *                Geters and seters(End)               *
+             *******************************************************/
+
+            /*******************************************************
+             *                Methods(Begin)                       *
+             *******************************************************/
+            public void Draw()
+            {
+
+            }
+
+        }//Node class.
+
+
+
+
+        public class Edge
+        {
+            Coordenate a;
+            Coordenate b;
+            int weight;
+            int direction;
+
+            public Edge()
+            {
+
+            }
+
+
+        }
+
+        public class Coordenate
+        {
+            int x;
+            int y;
+
+            public Coordenate(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public int X
+            {
+                get { return this.x; }
+                set { this.x = value; }
+            }
+
+            public int Y
+            {
+                get { return this.y; }
+                set { this.x = value; }
+            }
+        }
+
+        public class WeightMatrixGraph
+        {
+
+        }
+
+        public class adjacencyList
+        {
+
+        }
+
+
     }//Form.
-
-    public class Node
-    {
-        Coordenate position;
-        Color color;
-
-
-        public Node(Coordenate position)
-        {
-            this.position = position;
-        }
-
-        public void Draw()
-        {
-
-        }
-
-    }
-
-    public class Edge
-    {
-        Coordenate a;
-        Coordenate b;
-        int weight;
-        int direction;
-
-        public Edge()
-        {
-
-        }
-
-
-    }
-
-    public class Coordenate
-    {
-        int x;
-        int y;
-
-        public Coordenate(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
-    public class WeightMatrixGraph
-    {
-
-    }
-
-    public class adjacencyList
-    {
-
-    }
-
-
-
-
-
-
-
-
 }//namespace.
